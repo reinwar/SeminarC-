8add9a904438ab804571ed2b852d3d9725f78519
Aidar <strelokaar@gmail.com>
2022-06-17 02:02:33 +0500

    Семинар завершен!

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//int num;
+//Console.Write("Введите целое число: ");
+//num = Convert.ToInt32(Console.ReadLine());
+//num = num * num;
+//Console.WriteLine("Ваш результат: " + num);
+
+
+/*
+int num1, num2, num;
+Console.Write("Введите первое целое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+num = num2 * num2;
+
+if (num1 == num)
+{
+Console.WriteLine("Является");
+}
+else
+{
+  Console.WriteLine("не является");
+}
+*/
+
+//на вход принимает натуральное число, и в качестве результата выводит все числа от отрицательного версия этого числа до положительного !
+//натуральное число это - целое и положительное!
+/*
+int num;
+Console.Write("Введите первое целое число: ");
+num = Convert.ToInt32(Console.ReadLine());
+int current = num * (-1);
+while (current <= num)
+{
+Console.Write(current + " ");
+current++;  // current = current +1
+}
+*/
+св
