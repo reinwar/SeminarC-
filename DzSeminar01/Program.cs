a5e5f3429480eda2141667a80060408ce754fa26
Aidar <strelokaar@gmail.com>
2022-06-23 18:29:15 +0500

    Create folder programming

--- DzSeminar01/Program.cs
+++ DzSeminar01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+Console.WriteLine("Задача 4 ");
+Console.Write("Введи трёхзначное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(max);
+Console.WriteLine("Максимальное значение этого числа  -> "+stringNumber[1]);
+
+/*
+Console.Clear();
+Console.WriteLine("Задача 4 ");
+Console.Write("Введи трёхзначное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+*/
